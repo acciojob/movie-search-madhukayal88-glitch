d2de80f62c3c8f4abcb137116a4431cb1c7c782d
kayalvizhi <madhukayal88@gmail.com>
2026-08-07 10:34:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
+import React from 'react';
+import MovieSearch from './MovieSearch';
+import './styles.css';
 
-import React from "react";
-import './../styles/App.css';
-
-const App = () => {
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="app-container">
+      <MovieSearch />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
